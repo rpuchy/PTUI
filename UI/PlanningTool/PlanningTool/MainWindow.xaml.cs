a57eebe165a63240d9cc534c0da54b5e4820c19b
Rudi Puchy <Rudi Puchy>
2017-01-23 21:15:20 +1100

    added ui work

---
+++ UI/PlanningTool/PlanningTool/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace PlanningTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MenuItem root = new MenuItem() { Title = "Menu" };
+            MenuItem childItem1 = new MenuItem() { Title = "Child item #1" };
+            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.1" });
+            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.2" });
+            root.Items.Add(childItem1);
+            root.Items.Add(new MenuItem() { Title = "Child item #2" });
+            treeView1.Items.Add(root);
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            treeView1.Items.Add("Hello");
+            
+        }
+        
+        public class MenuItem
+        {
+            public MenuItem()
+            {
+                this.Items = new ObservableCollection<MenuItem>();
+            }
+
+            public string Title { get; set; }
+
+            public ObservableCollection<MenuItem> Items { get; set; }
+        }
+    }
+}
